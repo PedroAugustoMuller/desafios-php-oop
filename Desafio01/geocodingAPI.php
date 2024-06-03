6ffcc0edd18aec502f3db4e19078a6fe23089594
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-03 17:03:41 -0300

    Arquitetura de pastas

--- Desafio01/geocodingAPI.php
+++ Desafio01/geocodingAPI.php
@@ -1,7 +1,6 @@
 <?php
 
 function getCoordinates(string $city):array{
-    $city = 'Sao Paulo';
     $city = str_replace(" ","",$city);
     $apiResponse = "https://maps.googleapis.com/maps/api/geocode/json?address=$city&key=AIzaSyBwrdxGDMVSGdmEDZrKUge8v4_xVbdsKlA";
     $Geocode = json_decode(file_get_contents($apiResponse));
