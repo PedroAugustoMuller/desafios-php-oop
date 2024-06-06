cc1b204731ca122e8bf3414928fd85a03395fa03
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-06 18:21:22 -0300

    Email sem twig

--- Desafio01/src/service/WeatherReportEmail.php
+++ Desafio01/src/service/WeatherReportEmail.php
@@ -16,16 +16,36 @@ class WeatherReportEmail{
         $email = (new Email())
             ->from('hello@example.com')
             ->to($targetEmail)
-            ->subject("Clima". $weather->getCityInfo()." - " . $weather->getStringTime())
+            ->subject("Clima em: ". $weather->getCityInfo()." - " . $weather->getStringTime())
             ->text('')
-            ->html("<p class='location'><Cidade: ".$weather->getCityInfo()."</p>
-            <img src='https://openweathermap.org/img/wn/".$weather->getIcon()."@2x.png. 'alt=''>
-            <p class='temperature'>Temperatura: ". $weather->getTemp()."°C</p>
-            <p class='details'>Data: ".  $weather->getStringTime()."</p>
-            <p class='details'>Descrição: ". $weather->getDescription()."</p>
-            <p class='details'>Umidade: ". $weather->getHumidity()."%</p>
-            <p class='details'>Vento: ". $weather->getWindSpeed()."km/h</p>
-            <p class='details'>Sensação Térmica: ". $weather->getFeelsLike()."°C</p>");
+            // ->html("<p class='location'><Cidade: ".$weather->getCityInfo()."</p>
+            //         <img src='https://openweathermap.org/img/wn/".$weather->getIcon()."@2x.png. 'alt=''>
+            //         <p class='temperature'>Temperatura: ". $weather->getTemp()."°C</p>
+            //         <p class='details'>Data: ".  $weather->getStringTime()."</p>
+            //         <p class='details'>Descrição: ". $weather->getDescription()."</p>
+            //         <p class='details'>Umidade: ". $weather->getHumidity()."%</p>
+            //         <p class='details'>Vento: ". $weather->getWindSpeed()."km/h</p>
+            //         <p class='details'>Sensação Térmica: ". $weather->getFeelsLike()."°C</p>");
+            ->html("
+                    <div style ='max-width: 800px;margin: 50px auto;background: #ffffff8b;padding: 20px;border-radius: 10px;box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);display: grid;justify-content: center;backdrop-filter: blur(5px);'>
+                        <div style='text-align: center;'>
+                            <p style='font-size: 36px;margin-bottom: 15px;color: black;font-weight: bold;'>" . $weather->getCityInfo() ."</p>
+                        </div>
+                        <div style='display: grid;grid-auto-flow: column;text-align: center;column-gap: 100px;'>
+                            <div style ='display: grid;grid-auto-flow: column; column-gap: 10px;align-items: center;'>
+                                <img src='https://openweathermap.org/img/wn/".$weather->getIcon()."@2x.png' alt=''>
+                                <p style='font-size: 36px;font-weight: bold;color: #FFAE00;text-align: center;text-decoration: underline;'". $weather->getTemp(). "°C</p>
+                            </div>
+                            <div style='text-align: left;'>
+                                <p styles='font-size: 24px;margin-bottom: 10px;'>📔Data: ". $weather->getStringTime()."</p>
+                                <p styles='font-size: 24px;margin-bottom: 10px;'>⛅Descrição: " . $weather->getDescription() . "</p>
+                                <p styles='font-size: 24px;margin-bottom: 10px;'>🌫️Umidade: ". $weather->getHumidity() ."%</p>
+                                <p styles='font-size: 24px;margin-bottom: 10px;'>🌬️Vento: ". $weather->getWindSpeed(). "km/h</p>
+                                <p styles='font-size: 24px;margin-bottom: 10px;'>🌡️Sensação Térmica: ". $weather->getFeelsLike(). "°C</p>
+                            </div>
+                        </div>
+                    </div>"
+                );
 
         $mailer->send($email);
     }
