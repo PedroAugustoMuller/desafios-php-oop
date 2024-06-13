fa60e6aa23de5b2f3e8d80126c6bed3352be686e
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-06-13 01:47:45 -0300

    Testado

--- Desafio02/public/SQLScripts/ScriptRecente/script.sql
+++ Desafio02/public/SQLScripts/ScriptRecente/script.sql
@@ -54,22 +54,11 @@ CREATE TABLE items(
 INSERT INTO products (title,price,description,category,image) VALUES
         ('Solid Gold Petite Micropave ',168.00,'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.','jewelery','https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg'),
         ('White Gold Plated Princess',9.99,'Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine''s Day...','jewelery','https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg'),
-        ('MBJ Women''s Solid Short Sleeve Boat Neck V ',9.85,'95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem','women''s clothing','https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg'),
-		('Smartphone', 799.99, 'Um smartphone de última geração com câmera de alta resolução e desempenho rápido.', 'Eletrônicos', ''),
-		('Notebook', 1299.99, 'Um notebook potente com processador de última geração e tela de alta definição.', 'Eletrônicos', ''),
-		('Fone de Ouvido Bluetooth', 99.99, 'Fone de ouvido sem fio com cancelamento de ruído e bateria de longa duração.', 'Eletrônicos', ''),
-		('Smart TV', 899.99, 'Uma Smart TV com resolução 4K e acesso a vários aplicativos de streaming.', 'Eletrônicos', ''),
-		('Tablet', 299.99, 'Um tablet leve e portátil, perfeito para navegar na internet e consumir conteúdo multimídia.', 'Eletrônicos', ''),
-		('Tênis de Corrida', 129.99, 'Tênis de corrida com tecnologia de amortecimento avançada para máximo conforto durante a prática esportiva.', 'Esportes', ''),
-		('Bola de Futebol', 39.99, 'Bola de futebol profissional feita de material resistente para durabilidade e precisão.', 'Esportes', ''),
-		('Raquete de Tênis', 79.99, 'Raquete de tênis de alta qualidade com empunhadura ergonômica e excelente controle.', 'Esportes', ''),
-		('Bicicleta Mountain Bike', 599.99, 'Bicicleta mountain bike com suspensão dianteira e câmbio de marchas para enfrentar trilhas desafiadoras.', 'Esportes', ''),
-		('Bola de Basquete', 29.99, 'Bola de basquete oficial com design profissional e material durável.', 'Esportes', '');
-
-INSERT INTO ratings (rate,count,review_product_id) VALUES
-        (4.50,100,1),
-        (4.50,100,2),
-        (4.50,100,3);
+        ('MBJ Women''s Solid Short Sleeve Boat Neck V ',9.85,'95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem','women''s clothing','https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg');
+INSERT INTO ratings (rate, count, review_product_id) VALUES
+(4.50,100,1),
+(4.50,100,2),
+(4.50,100,3);
 
 INSERT INTO users(login, password,access) VALUES
 	('admin','$2y$10$F3VdekvJ7TKHZ8nlUu1Q.u7od.iU4WDwQ/dE3WGoGHpdg3fecI1/e',1),
